05e809db3abe1de69482bf6e013ae7946e4fc123
饒rao <jiahungrao@gmail.com>
2020-06-10 22:56:36 +0800

    Updates

---
+++ precache-manifest.af2e92d48e46d5eed2d830a0d032963c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63107821587ce619017933c06b45223a",
+    "url": "/AQUA/index.html"
+  },
+  {
+    "revision": "66ab868a55a4b9257790",
+    "url": "/AQUA/static/css/2.35da5615.chunk.css"
+  },
+  {
+    "revision": "27ee44a574f1f6d1f8c3",
+    "url": "/AQUA/static/css/main.d18bc767.chunk.css"
+  },
+  {
+    "revision": "66ab868a55a4b9257790",
+    "url": "/AQUA/static/js/2.ea1b425d.chunk.js"
+  },
+  {
+    "revision": "3b4181b60fc00975e0b659b57de93761",
+    "url": "/AQUA/static/js/2.ea1b425d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ee44a574f1f6d1f8c3",
+    "url": "/AQUA/static/js/main.817d2aa0.chunk.js"
+  },
+  {
+    "revision": "e00d0fea51b4860c0938",
+    "url": "/AQUA/static/js/runtime-main.bb720c52.js"
+  },
+  {
+    "revision": "e3b07ac7f2e1217cf44a92e9421758df",
+    "url": "/AQUA/static/media/20180615_201523.e3b07ac7.jpg"
+  },
+  {
+    "revision": "0406f6d59ff29172452ffee253f19159",
+    "url": "/AQUA/static/media/AQUA.0406f6d5.svg"
+  },
+  {
+    "revision": "3292c602b05be06a88fb6e4be8e3bf88",
+    "url": "/AQUA/static/media/ClassBanner.3292c602.jpg"
+  },
+  {
+    "revision": "70b69c3a4e58a4d163947757570f77fe",
+    "url": "/AQUA/static/media/L0020-1.70b69c3a.jpg"
+  },
+  {
+    "revision": "f1294bc201963202abd1ac9c3fa52ecd",
+    "url": "/AQUA/static/media/cancel.f1294bc2.svg"
+  },
+  {
+    "revision": "3b9608e410c55f9684aadc2de1b74800",
+    "url": "/AQUA/static/media/coralreef.3b9608e4.jpg"
+  },
+  {
+    "revision": "4e5f945f0ca3426cf8b75a10e80f1629",
+    "url": "/AQUA/static/media/givinBackground.4e5f945f.jpg"
+  },
+  {
+    "revision": "e39c695f1cd888b20e6863e51f17e708",
+    "url": "/AQUA/static/media/image.e39c695f.png"
+  },
+  {
+    "revision": "912f2373f6601bd1b53f7a636439511b",
+    "url": "/AQUA/static/media/sale-tag.912f2373.png"
+  },
+  {
+    "revision": "9eebcc30f3fcf8db1a5b5e6ccf2a85d6",
+    "url": "/AQUA/static/media/tickMenu.9eebcc30.svg"
+  },
+  {
+    "revision": "59f3d012236c5560c92ac87479dcde73",
+    "url": "/AQUA/static/media/user.59f3d012.svg"
+  }
+]);
